036081728eac2d3cc5eb9401cbda7a63b1a1fbeb
johngaynor <john.gaynor@irvmat.com>
2024-07-01 21:22:26 -0400

    created changelog route

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -13,6 +13,31 @@ router.get("/apps", async (req, res) => {
       `);
 
   res.status(200).json({ result });
+
+  router.get("/changelog", async (req, res) => {
+    const userId = req.user.id;
+    const pool = await poolPromise;
+    const [result] = await pool.query(`
+      select 
+        bld.version,
+        app.name,
+        bld.textId,
+        bld.text
+      from builds bld
+      left join buildUsers usr
+        on bld.version = usr.version
+      left join apps app
+        on bld.appId = app.id
+      left join apiUserAccess acc
+        on app.id = acc.appId
+      where 
+        usr.userId = '${userId}' and 
+        usr.userId = acc.userId and
+        bld.appId = acc.appId and 
+        usr.seen = 0
+      `);
+    res.status(200).json({ result });
+  });
 });
 
 module.exports = router;
