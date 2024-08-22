2b3eb3f0ed83d34d529fc2571f3a9cdb30f249f5
johngaynor <john.gaynor@irvmat.com>
2024-08-22 18:33:07 -0400

    combined workout log models

--- models/fitness/exerciseLog/index.js
+++ models/fitness/exerciseLog/index.js
@@ -36,14 +36,58 @@ const exerciseLogFunctions = {
     return new Promise(async function (resolve, reject) {
       try {
         const pool = await poolPromise;
-        const [result] = await pool.query(
+        // get summaries
+        const [summaries] = await pool.query(
           `
             select * from workoutLog
             where userId = ?
             `,
           [userId]
         );
-        resolve(result);
+
+        // get exercises
+        const [exercises] = await pool.query(
+          `
+            select ex.* from workoutExercises ex
+            left join workoutLog log 
+              on ex.workoutId = log.id
+            where log.userId = ?
+            `,
+          [userId]
+        );
+
+        // get sets
+        const [sets] = await pool.query(
+          `
+          select sets.* from workoutLogSets sets
+          left join workoutExercises ex 
+            on sets.workoutExerciseId = ex.id
+          left join workoutLog log
+            on ex.workoutId = log.id
+          where log.userId = ?
+          `,
+          [userId]
+        );
+
+        const logs = summaries.reduce((acc, val) => {
+          const retArr = [...acc];
+
+          const exerciseData = [];
+
+          exercises
+            .filter((e) => e.workoutId === val.id)
+            .forEach((e) => {
+              const matchingSets = sets.filter(
+                (s) => s.workoutExerciseId === e.id
+              );
+              exerciseData.push({ ...e, sets: matchingSets });
+            });
+
+          retArr.push({ ...val, exercises: exerciseData });
+
+          return retArr;
+        }, []);
+        resolve(logs);
       } catch (e) {
         reject(e);
       }
