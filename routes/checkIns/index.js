e894718c3e0b8b0e612960bbd24323af3285db84
johngaynor <john.gaynor@irvmat.com>
2024-08-31 14:54:51 -0400

    modified templates to remove an id

--- routes/checkIns/index.js
+++ routes/checkIns/index.js
@@ -4,6 +4,7 @@ const canAccess = require("../../models/middleware/canAccess");
 
 const canView = canAccess(5);
 
+// get check ins
 router.get("/", canView, async (req, res) => {
   try {
     const userId = req.user.id;
@@ -14,6 +15,39 @@ router.get("/", canView, async (req, res) => {
   }
 });
 
+// update check ins
+router.post("/", canView, async (req, res) => {
+  const userId = req.user.id;
+  // const {
+  //   id: workoutId,
+  //   date,
+  //   timeStarted,
+  //   timeCompleted,
+  //   comments,
+  // } = req.body;
+  console.log(req.body);
+
+  try {
+    // const method = await logFunctions.editWorkoutSummary(
+    //   workoutId,
+    //   userId,
+    //   date,
+    //   timeStarted,
+    //   timeCompleted,
+    //   comments
+    // );
+    // res.status(200).json({
+    //   message: `${
+    //     method === "insert" ? "inserted" : "updated"
+    //   } workout successfully`,
+    // });
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+// get templates
 router.get("/templates", canView, async (req, res) => {
   try {
     const result = await checkInFunctions.getCheckInsTemplates();
