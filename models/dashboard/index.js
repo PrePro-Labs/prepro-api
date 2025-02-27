a7bf1c032f397f757e9fdbdfea7ee72ac8b26f8f
johngaynor <john.gaynor@irvmat.com>
2025-02-27 09:53:16 -0500

    emergency to fix app crash

--- models/dashboard/index.js
+++ models/dashboard/index.js
@@ -1,6 +1,27 @@
 const { poolPromise } = require("../../config/database");
 
 const dashboardFunctions = {
+  async getApps(userId) {
+    return new Promise(async function (resolve, reject) {
+      try {
+        const pool = await poolPromise;
+        const [result] = await pool.query(
+          `
+          select app.* from apps app
+            left join apiUserAccess acc
+          on app.id = acc.appId
+          where acc.userId = ?
+            or app.allUsers = 1
+          `,
+          [userId]
+        );
+        resolve(result);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  },
+
   async updateChangeLogStatus(versionId, userId) {
     return new Promise(async function (resolve, reject) {
       try {
